5b6e487bd46419ce38f540723499b436c5705b58
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-04 13:43:56 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] array) {
+        int[] big_count = new int[array.length];
+        int[] small_count = new int[array.length];
+        int answer = 0;
+        for(int i = 0; i < array.length; i++){
+            big_count[i] = 0;
+            small_count[i] = 0;
+            for(int j = 0; j < array.length; j++){
+                if(array[j] > array[i]){
+                    big_count[i]++;
+                }
+                if(array[j] < array[i]){
+                    small_count[i]++;
+                }                
+            }
+            if(!(big_count[i] > (array.length / 2)) && !(small_count[i] > (array.length / 2))){
+                answer = array[i];
+                break;
+            }
+        }
+        return answer;
+    }
+}
