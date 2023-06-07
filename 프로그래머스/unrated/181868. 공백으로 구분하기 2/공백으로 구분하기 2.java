a67b2a9643dab450050887fd8991a1fa2aac5f54
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-07 18:40:10 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String my_string) {
+        while(my_string.contains("  ")){
+            my_string = my_string.replaceAll("  ", " ");
+        }
+        if(my_string.charAt(0) == ' '){
+            my_string = my_string.replaceFirst(" ", "");
+        }
+        return my_string.split(" ");
+    }
+}
