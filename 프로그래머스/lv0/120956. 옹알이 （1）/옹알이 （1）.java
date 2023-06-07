ed9dda509507b44b18723e72d6d40770e863c70f
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-07 14:38:31 +0900

    [level 0] Title: 옹알이 (1), Time: 1.11 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,19 +2,12 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for(String str : babbling) {
-            while(str.length() != 0){
-                if(str.startsWith("aya")){
-                    str = str.replaceFirst("aya", "");
-                } else if (str.startsWith("ye")){
-                    str = str.replaceFirst("ye", "");
-                } else if (str.startsWith("woo")){
-                    str = str.replaceFirst("woo", "");
-                } else if (str.startsWith("ma")){
-                    str = str.replaceFirst("ma", "");
-                } else {
-                    break;
-                }
-            }
+            str = str.replaceAll("aya", " ");
+            str = str.replaceAll("ye", " ");
+            str = str.replaceAll("woo", " ");
+            str = str.replaceAll("ma", " ");
+            str = str.replaceAll(" ", "");
+            
             if(str.length() == 0){
                 answer++;
             }
