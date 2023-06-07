b16ab121f21789c6ec8bf17172fbbeeefb8fb27a
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-07 10:59:18 +0900

    [unrated] Title: 카운트 다운, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start, int end) {
+        int length = start - end + 1;
+        int[] answer = new int[length];
+        for(int i = 0; i < length; i++){
+            answer[i] = start - i;
+        }
+        return answer;
+    }
+}
