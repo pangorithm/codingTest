04ed1d0cafb36e5c90f11537df971e873da36bf8
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-08 16:28:49 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        while(n_str.charAt(0) == '0'){
+            n_str = n_str.replaceFirst("0", "");
+        }
+        return n_str;
+    }
+}
