019e187077a36892dbe6d0ccb44c499e0a870314
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-12 09:54:20 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i = 1; fac(i) < n + 1; i++){
-            answer = i;
+        int fac = 1;
+        int x = 1;
+        while(fac < n + 1){
+            x++;
+            fac *= x;
         }
-        return answer;
-    }
-    
-    int fac(int x){
-        return x > 1 ? fac(x - 1) * x : 1;
+        return x - 1;
     }
 }
