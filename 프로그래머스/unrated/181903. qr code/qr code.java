a45401e3987871554b439b80fdc4ab23461d0b5e
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-13 19:26:57 +0900

    [unrated] Title: qr code, Time: 10.49 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.java
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(int q, int r, String code) {
-        char[] ca = new char[((code.length() - r + q - 1) / q)];
-        char[] codeArr = code.toCharArray();
-        for(int i = 0; i < ca.length; i++){
-            if((q * i + r) < codeArr.length){
-                ca[i] = codeArr[q * i + r];
+        String str = "";
+        for(int i = 0; i < code.length(); i++){
+            if(i % q == r){
+                str += code.charAt(i);
             }
         }
-        return String.valueOf(ca);
+        return str;
     }
 }
