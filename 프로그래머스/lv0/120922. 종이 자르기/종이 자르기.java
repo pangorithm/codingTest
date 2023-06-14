2ce7c3e3a2ab14f835d3784bf7ef6268320f2439
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-14 17:26:12 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int M, int N) {
-        if(M == 1){
-            if(N == 1){
-                return 0;
-            } else {
-                return Math.max((N - 1), 1);
-            } 
-        }
-        return Math.max((M - 1), 1) + M * Math.max((N - 1), 1);
+        return M * N - 1;
     }
 }
