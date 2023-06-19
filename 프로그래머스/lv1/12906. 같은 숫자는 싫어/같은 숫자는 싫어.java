80074c522abc516a17ebc3ce72e4c16e578245e7
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-19 20:26:25 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 26.66 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -10,7 +10,10 @@ public class Solution {
                 prev = arr[i];
             }
         }
-
-        return list.stream().mapToInt(i -> i).toArray();
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i).intValue();
+        }
+        return answer;
     }
 }
