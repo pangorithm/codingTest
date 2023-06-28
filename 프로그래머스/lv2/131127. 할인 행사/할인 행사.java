5764f3dd7d40216963fe46508aa5912512fcc109
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-28 09:33:29 +0900

    [level 2] Title: 할인 행사, Time: 0.12 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        HashMap<String, Integer> originMap = new HashMap<>();
+        for(int i = 0; i < want.length; i++){
+            originMap.put(want[i], number[i]);
+        }
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        int answer = 0;
+        loop : for(int day = 0; day < discount.length - want.length; day++){
+            for(String item : want){
+                map.put(item, originMap.get(item));
+            }
+            
+            for(int i = day; i < Math.min(day + 10, discount.length); i++){
+                if(map.getOrDefault(discount[i], 0) > 0){
+                    map.put(discount[i], map.get(discount[i]) - 1);
+                }
+            }
+            
+            for(String item : want){
+                if(map.get(item) > 0){
+                    continue loop;
+                }
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
