ff832d97397826f2b355f5f9b36f01af61fb89fe
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-06-30 15:38:19 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.17 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,49 +1,24 @@
-import java.util.HashSet;
-
 class Solution {
     public int solution(int n, int k) {
-        String toK = Integer.toString(n, k).replaceAll("0+", "0");
-        String[] arr = toK.split("0");
+        int answer = 0;
+        String[] arr = Integer.toString(n, k).replaceAll("0+", "0").split("0");
         if(arr.length == 0){
             return 0;
         }
-        int max = 0;
-        long[] plist = new long[arr.length];
-        for(int i = 0; i < arr.length; i++){
-            plist[i] = Long.parseLong(arr[i]);
-            if(plist[i] > Integer.MAX_VALUE){
-                max = 100;
-            } else if(max < plist[i]){
-                max = (int) plist[i];
-            }
-        }
         
-        HashSet<Integer> primeSet = new HashSet<>();
-        primeSet.add(2);
-        loop : for(int i = 3; i <= (max); i += 2){
-            for(int num : primeSet.toArray(new Integer[primeSet.size()])){
-                if(i % num == 0){
+        loop : for(String str : arr){
+            long temp = Long.parseLong(str);
+            if(temp < 2){
+                continue;
+            } else if(temp != 2 && (temp & 1) == 0){
+                continue;
+            }
+            for(int i = 3; i <= Math.sqrt(temp); i += 2){
+                if(temp % i == 0){
                     continue loop;
                 }
             }
-            primeSet.add(i);
-        }
-        // System.out.println(primeSet);
-        
-        int answer = 0;
-        
-        loop : for(long num : plist){
-            if(num > Integer.MAX_VALUE){
-                for(int prime : primeSet.toArray(new Integer[primeSet.size()])){
-                    if(num % prime == 0){
-                        continue loop;
-                    }
-                }
-                answer++;
-            } else if(primeSet.contains((int)num)){
-                answer++;
-            }
-        // System.out.println(num);
+            answer++;
         }
         
         return answer;
