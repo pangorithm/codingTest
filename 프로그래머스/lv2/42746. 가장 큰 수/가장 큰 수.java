08a899a1b400c290ea82b40774147f3dee552255
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-07-06 10:46:24 +0900

    [level 2] Title: 가장 큰 수, Time: 0.74 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strs = new String[numbers.length];
+        for(int i = 0; i < strs.length; i++){
+            strs[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(strs, (s1, s2) -> {
+            char[] chs1 = s1.toCharArray();
+            char[] chs2 = s2.toCharArray();
+            for(int i = 0; i < ((Math.min(chs1.length, chs2.length) << 1) + 1); i++){
+                if(chs1[i % chs1.length] != chs2[i % chs2.length]){
+                    return chs1[i % chs1.length] - chs2[i % chs2.length];
+                }
+            }
+            return chs2.length - chs1.length;                                                                 
+        });
+        
+        StringBuilder answer = new StringBuilder();
+        for(int i = strs.length - 1; i > -1; i--){
+            answer.append(strs[i]);  
+            // System.out.println(strs[i]);
+        }        
+        
+        String answerStr = answer.toString();
+        
+        for(char ch : answerStr.toCharArray()){
+            if(ch != '0'){
+                return answerStr;
+            }
+        }
+        
+        return "0";
+    }
+}
