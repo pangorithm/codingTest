047fdc4181851332fcf200ec5047907c6d30e8d2
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-07-09 13:11:55 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 7.40 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        long sum = 0;
+        long checkSum = 0;
+        int[] que = new int[queue1.length << 2];
+        for(int i = 0; i < queue1.length; i++){
+            que[i] = queue1[i];
+            que[queue1.length + i] = queue2[i];
+            que[(queue1.length << 1) + i] = queue1[i];
+            que[queue1.length * 3 + i] = queue2[i];
+            sum += queue1[i] + queue2[i];            
+            checkSum += queue1[i];
+        }
+        if((sum & 1) == 1){
+            return -1;
+        }
+        
+        int answer = 0;
+        sum >>= 1;
+        int start = 0;
+        int end = queue1.length;
+        while(start < (queue1.length << 1) && end < queue1.length * 3){
+            if(checkSum == sum){
+                return answer;
+            } else if(checkSum < sum){
+                checkSum += que[end];
+                end++;
+            } else if(checkSum > sum){
+                checkSum -= que[start];
+                start++;
+            }
+            
+            answer++;
+        }
+        
+        return -1;
+    }
+}
