0bd55dd2228307cfe9e3f7f7bb0ea95b44879267
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-07-10 17:38:04 +0900

    [level 2] Title: [3차] 방금그곡, Time: 18.18 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,21 +1,12 @@
 class Solution {
     public String solution(String m, String[] musicinfos) {
         String answer = "(None)";
+        m = replaceMelody(m);
         int maxPlayTime = 0;
+        
         for(String str : musicinfos){
             String[] splited = str.split(","); // {start, end, title, sheet}
-            splited[3] = splited[3]
-                .replaceAll("C#", "H")
-                .replaceAll("D#", "I")
-                .replaceAll("F#", "J")
-                .replaceAll("G#", "K")
-                .replaceAll("A#", "L");
-            m = m
-                .replaceAll("C#", "H")
-                .replaceAll("D#", "I")
-                .replaceAll("F#", "J")
-                .replaceAll("G#", "K")
-                .replaceAll("A#", "L");
+            splited[3] = replaceMelody(splited[3]);
             String[] start = splited[0].split(":");
             String[] end = splited[1].split(":");
             int playTime = (Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1])) 
@@ -24,14 +15,24 @@ class Solution {
                 .repeat((playTime / splited[3].length()) + 1)
                 .substring(0, playTime);
             // System.out.println(playedMelody);
+            
             if(playedMelody.contains(m)){
                 if(playTime > maxPlayTime){
                     answer = splited[2];
                     maxPlayTime = playTime;
                 }
-            }
-                
+            }                
         }
+        
         return answer;
+    }
+    
+    String replaceMelody (String melody){
+        return melody
+            .replaceAll("C#", "H")
+            .replaceAll("D#", "I")
+            .replaceAll("F#", "J")
+            .replaceAll("G#", "K")
+            .replaceAll("A#", "L");
     }
 }
