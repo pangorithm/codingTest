5bbd59ea4dfa7f57cd6f2c1e65e5d3e097ab9232
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-07-17 16:26:44 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 9.03 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        long rr1 = (long) r1 * r1;
+        long rr2 = (long) r2 * r2;
+        
+        int xMin = r1;
+        int xMax = r2;
+        loopY : for(int y = 0; y < r2; y++){
+            while(xMin > 0 && (long) y * y + (long)xMin * xMin >= rr1){
+                xMin--;
+            }
+            while(xMax > 0 && (long) y * y + (long) xMax * xMax > rr2){
+                xMax--;
+            }
+            if(xMax > xMin){
+                answer += xMax - xMin;
+            }
+            // System.out.println(y+" "+xMin +" "+ xMax);
+        }
+        return answer << 2;
+    }
+}
