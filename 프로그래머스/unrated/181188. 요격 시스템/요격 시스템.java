0e2b51a0cd55218974345bff919656e397f2e30a
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-07-20 16:15:09 +0900

    [unrated] Title: 요격 시스템, Time: 0.56 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
-        Arrays.sort(targets, (v1, v2) -> v1[1] - v2[1]);
+        Arrays.sort(targets, (v1, v2) -> v1[1] - v2[1]); // 미사일 착탄지점 기준 정렬
         int answer = 0;
-        int cursor = 0;
+        int cursor = 0; // 요격 시스템 위치
         
         for(int[] target : targets){
-            if(target[0] < cursor){
+            if(target[0] < cursor){ // 현재 요격시스템 위치에서 해결 가능할 경우
                 continue;
-            } else {
+            } else { // 그렇지 않을 경우 요격 시스템 범위 추가
                 cursor = target[1];
                 answer++;
             }
