607c7399248b56a1432e5f4e434a5a42ee7f253e
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-07-25 09:42:04 +0900

    [unrated] Title: 유사 칸토어 비트열, Time: 5.66 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int n, long l, long r) {
         int answer = 0;
-        int length = (int) Math.pow(5, n);
-        int allOneCount = (int) Math.pow(4, n);
         char[] chs = cantor(n,l - 1,r - 1).toCharArray();
         for(char ch : chs){
             if(ch == '1'){
@@ -16,9 +14,7 @@ class Solution {
         if(n == 0){
             return "1";
         }
-        String str =  cantor(n - 1, l / 5, r / 5).replaceAll("0", "00000").replaceAll("1", "11011");                   str = str.substring((int) (l % 5), str.length() - (5 - (int) (r % 5)) + 1);
-        // System.out.println(str);
-        return str;
+        String str =  cantor(n - 1, l / 5, r / 5).replaceAll("0", "00000").replaceAll("1", "11011");                   return str.substring((int) (l % 5), str.length() - (5 - (int) (r % 5)) + 1);
     }
 }
 
