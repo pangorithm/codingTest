c239b2c7d2a748527d1182f8194fce08d265100c
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-07-26 19:17:10 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] str) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));) {
+      String[] inStrs = br.readLine().split(" ");
+      bw.write(Integer.toString(Integer.parseInt(inStrs[0]) - Integer.parseInt(inStrs[1])));
+    } catch (IOException ioE) {
+      // TODO: handle exception
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
