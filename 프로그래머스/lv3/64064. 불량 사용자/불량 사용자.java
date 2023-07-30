6f14f7d83d14dc99d39a410fc77237b1bc7cb28d
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-07-30 16:44:15 +0900

    [level 3] Title: 불량 사용자, Time: 0.33 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
@@ -2,43 +2,28 @@ import java.util.*;
 
 class Solution {
     static HashSet<HashSet<String>> answerSet;
-    static HashMap<String, HashSet<String>> bannedIdMap;
     
     public int solution(String[] user_id, String[] banned_id) {
-        bannedIdMap = new HashMap<>();
-        for(String banned : banned_id){
-            if(!bannedIdMap.containsKey(banned)){
-                HashSet<String> set = new HashSet<>();
-                for(String user : user_id){
-                    if(user.length() == banned.length() && canTrance(user, banned)){
-                        set.add(user);
-                    }
-                }
-                bannedIdMap.put(banned, set);
-            }
-        }
-        // System.out.println(bannedIdMap);
-        
         answerSet = new HashSet<HashSet<String>>();
-        search(new HashSet<String>(), 0, banned_id);
+        search(new HashSet<String>(), 0, user_id, banned_id);
         
         return answerSet.size();
     }
     
-    void search(HashSet<String> userSet, int dept, String[] banned_id){
+    void search(HashSet<String> userSet, int dept, String[] user_id, String[] banned_id){
         if(dept == banned_id.length){
             if(userSet.size() == dept){
                 // System.out.println(userSet);
                 answerSet.add(userSet);
             }            
         } else {
-            HashSet<String> set = bannedIdMap.get(banned_id[dept]);
-            String[] arr = set.toArray(new String[set.size()]);
-            for(String user : arr){
-                if(!userSet.contains(user)){
+            for(String user : user_id){
+                if(user.length() == banned_id[dept].length() 
+                   && canTrance(user, banned_id[dept])
+                   && !userSet.contains(user)){
                     HashSet<String> newUserSet = new HashSet<>(userSet);
                     newUserSet.add(user);
-                    search(newUserSet, dept + 1, banned_id);
+                    search(newUserSet, dept + 1, user_id, banned_id);
                 }                
             }
         }
