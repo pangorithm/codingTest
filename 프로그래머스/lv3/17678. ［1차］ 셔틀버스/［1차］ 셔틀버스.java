43d3ce0057f1ab18550f57de21d037f79a8688a3
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-08-08 11:38:49 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 5.72 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -5,24 +5,21 @@ class Solution {
         String lastTime = String.format("%02d:%02d", 9 + ((n - 1) * t) / 60, ((n - 1) * t) % 60);        
         String answer = lastTime;
         
-        ArrayList<String> timeList = new ArrayList<>(Arrays.asList(timetable));
-        Collections.sort(timeList);
+        Arrays.sort(timetable);
+        int cursor = 0;
         
         for(int i = 0; i < n; i++){
             int count = 0;
             String busTime = String.format("%02d:%02d", 9 + (i * t) / 60, (i * t) % 60);
-            // System.out.println(busTime);
             
-            while(timeList.size() > 0){
-                if(timeList.get(0).compareTo(busTime) > 0){
+            while(cursor < timetable.length){
+                if(timetable[cursor].compareTo(busTime) > 0){
                     break;
-                }
-                // System.out.println(timeList.get(0)+" "+timeMap.get(timeList.get(0)));
-                
+                }                
                 count ++;
                 
                 if(i == n - 1 && count >= m){
-                    String[] hnm = timeList.get(0).split(":");
+                    String[] hnm = timetable[cursor].split(":");
                     int hour = Integer.parseInt(hnm[0]);
                     int min = Integer.parseInt(hnm[1]) - 1;
                     if(min < 0){
@@ -34,7 +31,7 @@ class Solution {
                 }
                 
                 if(count <= m){
-                    timeList.remove(0);
+                    cursor++;
                 } else {
                     break;
                 }
