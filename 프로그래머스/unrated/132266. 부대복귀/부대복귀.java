50c8d1143eb751b271ce09e545531e3a43ef3608
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-08-13 13:40:36 +0900

    [unrated] Title: 부대복귀, Time: 50.92 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/unrated/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
 class Solution {
-    static HashMap<Integer, ArrayList<Integer>> roadMap;
+    static ArrayList<Integer>[] loadList;
     static int[] dp;
     
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        roadMap = new HashMap<Integer, ArrayList<Integer>>();
+        loadList = new ArrayList[n + 1];
         dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[destination] = 0;
         
         for(int[] r : roads){
-            ArrayList<Integer> list = roadMap.getOrDefault(r[0], new ArrayList<Integer>());
+            if(loadList[r[0]] == null){
+                loadList[r[0]] = new ArrayList<Integer>();
+            }
+            ArrayList<Integer> list = loadList[r[0]];
             list.add(r[1]);
-            roadMap.put(r[0], list);
             
-            list = roadMap.getOrDefault(r[1], new ArrayList<Integer>());
+            if(loadList[r[1]] == null){
+                loadList[r[1]] = new ArrayList<Integer>();
+            }
+            list = loadList[r[1]];
             list.add(r[0]);
-            roadMap.put(r[1], list);
         }
         
         Queue<State> bfs = new ArrayDeque<State>();
@@ -26,8 +30,8 @@ class Solution {
         while(bfs.size() > 0){
             State state = bfs.poll();
             
-            if(roadMap.containsKey(state.loc)){
-                ArrayList<Integer> list = roadMap.get(state.loc);
+            if(loadList[state.loc] != null){
+                ArrayList<Integer> list = loadList[state.loc];
                 int length = list.size();
                 for(int i = 0; i < length; i++){
                     if(dp[list.get(i)] > state.dst && state.dst < n){
