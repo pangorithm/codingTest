2e4b9e46afcf8a1e1ea17ee0fc82303eef0e78b0
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-08-21 13:52:13 +0900

    [level 3] Title: 외벽 점검, Time: 1.70 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/60062. 외벽 점검/외벽 점검.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+    static int answer = -1;
+    
+    public int solution(int n, int[] weak, int[] dist) {
+        int[] dumpWeak = new int[weak.length << 1];
+        for(int i = 0; i < weak.length; i++){
+            dumpWeak[i] = weak[i];
+            dumpWeak[i + weak.length] = weak[i] + n;
+        }
+        
+        Integer[] sortedDist = new Integer[dist.length];
+        for(int i = 0; i < dist.length; i++){
+            sortedDist[i] = dist[i];
+        }
+        Arrays.sort(sortedDist, (v1, v2) -> v2 - v1);
+        
+        Queue<State> bfs = new ArrayDeque<>();
+        bfs.offer(new State(new boolean[weak.length], 0));
+        
+        while(bfs.size() > 0){
+            State state = bfs.poll();
+
+            for(int i = 0; i < weak.length; i++){
+                if(!state.weakState[i]){
+                    boolean[] nextWeakState = Arrays.copyOf(state.weakState, weak.length);
+                    for(int j = i; dumpWeak[j] <= weak[i] + sortedDist[state.dept]; j++){
+                        nextWeakState[j % weak.length] = true;
+                    }
+                    // System.out.println(Arrays.toString(nextWeakState)+" "+(state.dept + 1));
+                                    
+                    boolean checkedAll = true;
+                    for(boolean b : nextWeakState){
+                        // System.out.println(Arrays.toString(state.weakState));
+                        if(!b){
+                            checkedAll = false;
+                            break;
+                        }
+                    }
+                    if(checkedAll){
+                        return state.dept + 1;
+                    } 
+                        
+                    if(state.dept + 1 < dist.length){
+                        bfs.offer(new State(nextWeakState, state.dept + 1));
+                    }
+                }
+            }
+
+        }
+        
+        return answer;
+    }
+    
+    class State{
+        boolean[] weakState;
+        int dept;
+        
+        State(boolean[] weakState, int dept){
+            this.weakState = weakState;
+            this.dept = dept;
+        }
+    }
+}
