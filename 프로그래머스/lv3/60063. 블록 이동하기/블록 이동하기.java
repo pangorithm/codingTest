1da85aede6a5988c91e692da48b90e66d37a479a
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-08-27 14:46:19 +0900

    [level 3] Title: 블록 이동하기, Time: 38.90 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.java
@@ -28,109 +28,88 @@ class Solution {
             if(state.isVertical){ // 로봇이 세로일 경우
                 // 이동
                 State ns = new State(state.y + 1, state.x, state.isVertical); // 아래로 이동
-                if(boolBoard[ns.y + 1][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y - 1, state.x, state.isVertical); // 위로 이동
-                if(boolBoard[ns.y][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y, state.x + 1, state.isVertical); // 우로 이동
-                if(boolBoard[ns.y][ns.x] && boolBoard[ns.y + 1][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y, state.x - 1, state.isVertical); // 좌로 이동
-                if(boolBoard[ns.y][ns.x] && boolBoard[ns.y + 1][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 // 회전
                 ns = new State(state.y, state.x - 1, !state.isVertical); // 좌상단 회전
-                if(boolBoard[ns.y][ns.x] && boolBoard[ns.y + 1][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y, state.x, !state.isVertical); // 우상단 회전
-                if(boolBoard[ns.y][ns.x + 1] && boolBoard[ns.y + 1][ns.x + 1] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y + 1, state.x - 1, !state.isVertical); // 좌하단 회전
-                if(boolBoard[ns.y][ns.x] && boolBoard[ns.y - 1][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y + 1, state.x, !state.isVertical); // 우하단 회전
-                if(boolBoard[ns.y][ns.x + 1] && boolBoard[ns.y - 1][ns.x + 1] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
+                valid(state, ns, nsTime);
             } else { // 로봇이 가로일 경우
                 // 이동
                 State ns = new State(state.y, state.x + 1, state.isVertical); // 우로 이동
-                if(boolBoard[ns.y][ns.x + 1] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y, state.x - 1, state.isVertical); // 좌로 이동
-                if(boolBoard[ns.y][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y + 1, state.x, state.isVertical); // 아래로 이동
-                if(boolBoard[ns.y][ns.x] && boolBoard[ns.y][ns.x + 1] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y - 1, state.x, state.isVertical); // 위로 이동
-                if(boolBoard[ns.y][ns.x] && boolBoard[ns.y][ns.x + 1] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);                
                 // 회전
                 ns = new State(state.y - 1, state.x, !state.isVertical); // 좌상단 회전
-                if(boolBoard[ns.y][ns.x] && boolBoard[ns.y][ns.x + 1] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y - 1, state.x + 1, !state.isVertical); // 우상단 회전
-                if(boolBoard[ns.y][ns.x] && boolBoard[ns.y][ns.x - 1] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y, state.x, !state.isVertical); // 좌하단 회전
-                if(boolBoard[ns.y + 1][ns.x + 1] && boolBoard[ns.y + 1][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);
                 ns = new State(state.y, state.x + 1, !state.isVertical); // 우하단 회전
-                if(boolBoard[ns.y + 1][ns.x - 1] && boolBoard[ns.y + 1][ns.x] && nsTime < visitMap.getOrDefault(ns, 100000)){
-                    visitMap.put(ns, nsTime);
-                    bfs.offer(ns);
-                }
-                
+                valid(state, ns, nsTime);                
             }
         }
         
         return Math.min(visitMap.getOrDefault(new State(board.length, board[0].length - 1, false), 100000),
                        visitMap.getOrDefault(new State(board.length - 1, board[0].length, true), 100000));
+    }
+    
+    void valid(State s, State ns, int nsTime){
+        if(s.isVertical == ns.isVertical){
+            if(ns.isVertical){
+                if(!boolBoard[ns.y][ns.x] || !boolBoard[ns.y + 1][ns.x]){
+                    return;
+                }
+            } else {
+                if(!boolBoard[ns.y][ns.x] || !boolBoard[ns.y][ns.x + 1]){
+                    return;
+                }
+            }
+        } else {
+            int sx1 = s.x, sy1 = s.y, sx2 = s.isVertical ? s.x : s.x + 1, sy2 = s.isVertical ? s.y + 1 : s.y;
+            int nsx1 = ns.x, nsy1 = ns.y, nsx2 = ns.isVertical ? ns.x : ns.x + 1, nsy2 = ns.isVertical ? ns.y + 1 : ns.y;
+            for(int y = Math.min(sy1, nsy1); y <= Math.max(sy2, nsy2); y++){
+                for(int x = Math.min(sx1, nsx1); x <= Math.max(sx2, nsx2); x++){
+                    if(!boolBoard[y][x]){
+                        return;
+                    }
+                }
+            }
+        }
+        
+        if(nsTime < visitMap.getOrDefault(ns, 100000)){
+            visitMap.put(ns, nsTime);
+            bfs.offer(ns);
+        }
+    }
+    
+    boolean validNS(State ns){
+        if(ns.isVertical){
+            if(boolBoard[ns.y][ns.x] && boolBoard[ns.y + 1][ns.x]){
+                return true;
+            }
+        } else {
+            if(boolBoard[ns.y][ns.x] && boolBoard[ns.y][ns.x + 1]){
+                return true;
+            }
+        }
+        return false;
     }
     
     class State{
