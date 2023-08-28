78fec58bc5819ee3a23bbf71306e0620c30e0cbc
pangorithm <phs559800@gmail.com>
2023-08-28 21:11:15 +0900

    백준 입출력 양식 업데이트

--- 백준/Main.java
+++ 백준/Main.java
@@ -4,23 +4,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  static StringBuilder answer = new StringBuilder();
 
   public static void main(String[] args) throws IOException {
     // 한 라인에 여러 입력이 있을 경우
-    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringTokenizer st = new StringTokenizer(in.readLine());
 
     // 계속해서 라인으로 입력을 받을 경우
     String str;
-    while((str = br.readLine()) != null){
-      if(str.length() == 0){ break; } // 백준에서는 불필요한 코드. 제출 시 주석처리 할 것
+    while((str = in.readLine()) != null){
+      if(str.length() == 0){ break; } // 백준에서는 파일 입력이므로 불필요한 코드. 제출 시 주석처리 할 것
       int n = Integer.parseInt(str);
-      bw.write(n + "\n");
+      answer.append(n).append("\n");
     }
 
-    ///////////
-    bw.flush();
+    /////////////////////////
+    System.out.print(answer);
     bw.close();
     br.close();
   }
