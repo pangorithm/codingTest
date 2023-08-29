17c6f2fe93fe48a6a2346f742cc6cbdad7efb207
pangorithm <phs559800@gmail.com>
2023-08-29 10:02:23 +0900

    백준 제출 양식 업데이트

--- 백준/Main.java
+++ 백준/Main.java
@@ -5,18 +5,24 @@ import java.util.*;
 
 public class Main {
   static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-  static StringBuilder answer = new StringBuilder();
 
   public static void main(String[] args) throws IOException {
 
+    StringBuilder sb = new StringBuilder();
+    sb.append("test");
+    System.out.println(sb);
 
-    /////////////////////////
-    System.out.print(answer);
     in.close();
   }
 }
 
-/*
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+private class MemoForMe{
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static StringBuilder sb = new StringBuilder(); // 긴 결과 제출 시 시용할것
+
+  public static void memo(String[] args) throws IOException {
+
     // 한 라인에 하나의 변수가 있을 경우
     int n = Integer.parseInt(in.readLine());
 
@@ -27,7 +33,11 @@ public class Main {
     String str;
     while((str = in.readLine()) != null){
       if(str.length() == 0){ break; } // 백준에서는 파일 입력이므로 불필요한 코드. 제출 시 주석처리 할 것
-      int n = Integer.parseInt(str);
-      answer.append(n).append("\n");
+      int x = Integer.parseInt(str);
+      sb.append(n).append("\n");
     }
-*/
+
+    // toString() 없이 제출 가능
+    System.out.pirnt(sb);
+  }
+}
