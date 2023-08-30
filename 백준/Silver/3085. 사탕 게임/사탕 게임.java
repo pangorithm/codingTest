e821a431bcb4c2ea91b543ccdac6dffbc41050a1
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-08-30 14:32:27 +0900

    [Silver II] Title: 사탕 게임, Time: 292 ms, Memory: 14900 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  static char[][] board;
+  static int n;
+
+  public static void main(String[] args) throws IOException {
+
+    n = Integer.parseInt(in.readLine());
+    board = new char[n + 3][n + 3];
+    for (int i = 1; i <= n; i++) {
+      char[] temp = in.readLine().toCharArray();
+      for (int j = 0; j < n; j++) {
+        board[i][j + 1] = temp[j];
+      }
+    }
+
+    int max = 0;
+    char temp = board[1][1];
+    board[1][1] = board[1][2];
+    board[1][2] = temp;
+    int count = check();
+    if (max < count) {
+      max = count;
+    }
+    board[1][2] = board[1][1];
+    board[1][1] = board[2][1];
+    board[2][1] = temp;
+    count = check();
+    if (max < count) {
+      max = count;
+    }
+    board[2][1] = board[1][1];
+    board[1][1] = temp;
+
+    for (int i = 2; i <= n; i++) {
+      for (int j = 2; j <= n; j++) {
+        temp = board[i][j];
+        board[i][j] = board[i - 1][j];
+        board[i - 1][j] = temp;
+        count = check();
+        if (max < count) {
+          max = count;
+        }
+        board[i - 1][j] = board[i][j];
+        board[i][j] = board[i][j - 1];
+        board[i][j - 1] = temp;
+        count = check();
+        if (max < count) {
+          max = count;
+        }
+        board[i][j - 1] = board[i][j];
+        board[i][j] = temp;
+      }
+    }
+
+    System.out.println(max);
+    in.close();
+  }
+
+  static private int check() {
+    int max = 0;
+    int count = 0;
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 2; j <= n + 1; j++) { // 행 체크
+        if (board[i][j] != board[i][j - 1]) {
+          count = 0;
+          for (int k = j - 1; k > 0 && board[i][k] == board[i][j - 1]; k--) {
+            count++;
+          }
+          if (max < count) {
+            max = count;
+          }
+        }
+      }
+    }
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 2; j <= n + 1; j++) { // 열 체크
+        if (board[j][i] != board[j - 1][i]) {
+          count = 0;
+          for (int k = j - 1; k > 0 && board[k][i] == board[j - 1][i]; k--) {
+            count++;
+          }
+          if (max < count) {
+            max = count;
+          }
+        }
+      }
+    }
+
+    return max;
+  }
+}
