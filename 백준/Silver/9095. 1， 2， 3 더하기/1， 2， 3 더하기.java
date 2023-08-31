11d314c4cc458041a5b194819854818ede44a742
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-08-31 20:53:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -8,24 +8,28 @@ public class Main {
   public static void main(String[] args) throws IOException {
 
     int T = Integer.parseInt(in.readLine());
+    int[] answer = new int[T];
+    int max = 0;
     for (int i = 0; i < T; i++) {
-      int n = Integer.parseInt(in.readLine());
-      sb.append(search(n, 0)).append("\n");
+      answer[i] = Integer.parseInt(in.readLine());
+      if (max < answer[i]) {
+        max = answer[i];
+      }
+    }
+
+    int[] dp = new int[max + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i = 4; i <= max; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+
+    for (int i = 0; i < T; i++) {
+      sb.append(dp[answer[i]]).append("\n");
     }
 
     System.out.println(sb);
     in.close();
-  }
-
-  private static int search(int n, int sum) {
-    int count = 0;
-    if (sum == n) {
-      return 1;
-    } else if (sum < n) {
-      for (int i = 1; i < 4; i++) {
-        count += search(n, sum + i);
-      }
-    }
-    return count;
   }
 }
