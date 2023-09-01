46a9a15157c4ada61c7eb38e408f71734661a1ff
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-09-01 09:49:40 +0900

    [Silver III] Title: N과 M (7), Time: 804 ms, Memory: 206624 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  static StringBuilder sb = new StringBuilder();
+  static int N;
+  static int M;
+  static int[] numArr;
+
+  public static void main(String[] args) throws IOException {
+
+    StringTokenizer st = new StringTokenizer(in.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    numArr = new int[N];
+
+    st = new StringTokenizer(in.readLine());
+    for (int i = 0; i < N; i++) {
+      numArr[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(numArr);
+
+    dfs(new StringBuilder(), 0);
+
+    System.out.println(sb);
+    in.close();
+  }
+
+  static void dfs(StringBuilder str, int dept) {
+    if (dept == M) {
+      sb.append(str).append("\n");
+    } else {
+      for (int i = 0; i < N; i++) {
+        dfs(new StringBuilder(str).append(numArr[i]).append(" "), dept + 1);
+      }
+    }
+  }
+}
