a2c7239987af72efd709cec680a462e4699270a4
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-09-01 16:08:35 +0900

    [Silver I] Title: NM과 K (1), Time: 1796 ms, Memory: 339236 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -15,18 +15,14 @@ public class Main {
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
     K = Integer.parseInt(st.nextToken());
-    board = new Cell[N + 2][M + 2];
+    board = new Cell[N + 1][M + 1];
     max = Integer.MIN_VALUE;
 
-    int lastN = N + 1;
-    int lastM = M + 1;
-    for (int i = 0; i <= lastN; i++) {
+    for (int i = 0; i <= N; i++) {
       board[i][0] = new Cell();
-      board[i][lastM] = new Cell();
     }
-    for (int j = 0; j <= lastM; j++) {
+    for (int j = 0; j <= M; j++) {
       board[0][j] = new Cell();
-      board[lastN][j] = new Cell();
     }
     for (int i = 1; i <= N; i++) {
       st = new StringTokenizer(in.readLine());
@@ -54,16 +50,25 @@ public class Main {
       dept++;
 
       for (int j = startJ + 2; j <= M; j++) {
-        if (!set.contains(board[startI - 1][j]) && !set.contains(board[startI][j - 1])) {
+        if (!set.contains(board[startI - 1][j])) {
           HashSet<Cell> nextSet = new HashSet<>(set);
           nextSet.add(board[startI][j]);
           dfs(nextSet, dept, startI, j);
         }
       }
 
-      for (int i = startI + 1; i <= N; i++) {
+      int tempI = startI + 1;
+      if (tempI <= N) {
         for (int j = 1; j <= M; j++) {
-          if (!set.contains(board[i - 1][j]) && !set.contains(board[i][j - 1])) {
+          if (!set.contains(board[tempI - 1][j])) {
+            HashSet<Cell> nextSet = new HashSet<>(set);
+            nextSet.add(board[tempI][j]);
+            dfs(nextSet, dept, tempI, j);
+          }
+        }
+
+        for (int i = startI + 2; i <= N; i++) {
+          for (int j = 1; j <= M; j++) {
             HashSet<Cell> nextSet = new HashSet<>(set);
             nextSet.add(board[i][j]);
             dfs(nextSet, dept, i, j);
