e80b7f8fb396c5fe57f3431bd5831a468b61c1d5
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-09-01 09:27:45 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 15988 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  static StringBuilder sb = new StringBuilder();
+  static int N;
+  static int M;
+
+  public static void main(String[] args) throws IOException {
+
+    StringTokenizer st = new StringTokenizer(in.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    dfs(new StringBuilder(), 0, 1);
+
+    System.out.println(sb);
+    in.close();
+  }
+
+  static void dfs(StringBuilder str, int dept, int start) {
+    if (dept == M) {
+      sb.append(str).append("\n");
+    } else {
+      for (int i = start; i <= N; i++) {
+        dfs(new StringBuilder(str).append(i).append(" "), dept + 1, i);
+      }
+    }
+  }
+}
