3f9da506b465eeef5dc5caf39bacd061f8c39f77
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-09-05 09:33:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 140 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+  public static void main(String[] args) throws IOException {
+
+    int N = Integer.parseInt(in.readLine());
+    int[] p = new int[N + 1];
+    StringTokenizer st = new StringTokenizer(in.readLine());
+    for (int i = 1; i <= N; i++) {
+      p[i] = Integer.parseInt(st.nextToken());
+    }
+
+    for (int i = 1; i < N; i++) {
+      for (int j = i; j < N; j++) {
+        if (i + j <= N) {
+          if (p[i + j] < p[i] + p[j]) {
+            p[i + j] = p[i] + p[j];
+          }
+        }
+      }
+    }
+
+    System.out.println(p[N]);
+    in.close();
+  }
+}
