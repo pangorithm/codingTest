21effd20354bde1ef4dc5e50b762fc10374164ae
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-09-07 21:07:59 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 32.39 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,12 +1,18 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
-        for(int i = 0; i < queries.length; i++){
-            my_string = 
-                new StringBuilder(my_string.substring(queries[i][0], queries[i][1] + 1))
-                .reverse()
-                .insert(0, my_string.substring(0, queries[i][0]))
-                .append(my_string.substring(queries[i][1] + 1))
-                .toString();
+        for(int[] query : queries){
+            char[] chars = my_string.toCharArray();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < query[0]; i++){
+                sb.append(chars[i]);
+            }
+            for(int i = query[1]; i >= query[0]; i--){
+                sb.append(chars[i]);
+            }
+            for(int i = query[1] + 1; i < chars.length; i++){
+                sb.append(chars[i]);
+            }
+            my_string = sb.toString();
         }
         return my_string;
     }
