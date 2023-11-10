ad59ae69c62acab87811fded074c3a502cf1cb84
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-11-10 17:46:09 +0900

    [Gold V] Title: 적록색약, Time: 172 ms, Memory: 16068 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -8,20 +8,25 @@ public class Main {
 
     int N = Integer.parseInt(br.readLine());
     char[][] board = new char[N][N];
-    boolean[][] visit = new boolean[N][N];
+    boolean[][] rgbVisit = new boolean[N][N];
+    boolean[][] rbVisit = new boolean[N][N];
+
     for (int i = 0; i < N; i++) {
       board[i] = br.readLine().toCharArray();
     }
+    br.close();
+
     int[][] moves = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
 
     Queue<int[]> bfs = new ArrayDeque<>();
 
     int rgbCount = 0;
+    int rbCount = 0;
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
-        if (!visit[i][j]) {
+        if (!rgbVisit[i][j]) {
           rgbCount++;
-          visit[i][j] = true;
+          rgbVisit[i][j] = true;
           bfs.offer(new int[] { i, j });
           char color = board[i][j];
           while (bfs.size() > 0) {
@@ -29,23 +34,17 @@ public class Main {
             for (int[] m : moves) {
               int ny = loc[0] + m[0];
               int nx = loc[1] + m[1];
-              if (ny >= 0 && ny < N && nx >= 0 && nx < N && !visit[ny][nx] && board[ny][nx] == color) {
-                visit[ny][nx] = true;
+              if (ny >= 0 && ny < N && nx >= 0 && nx < N && !rgbVisit[ny][nx] && board[ny][nx] == color) {
+                rgbVisit[ny][nx] = true;
                 bfs.offer(new int[] { ny, nx });
               }
             }
           }
         }
-      }
-    }
 
-    visit = new boolean[N][N];
-    int rbCount = 0;
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (!visit[i][j]) {
+        if (!rbVisit[i][j]) {
           rbCount++;
-          visit[i][j] = true;
+          rbVisit[i][j] = true;
           bfs.offer(new int[] { i, j });
           char color = board[i][j];
           while (bfs.size() > 0) {
@@ -53,20 +52,20 @@ public class Main {
             for (int[] m : moves) {
               int ny = loc[0] + m[0];
               int nx = loc[1] + m[1];
-              if (ny >= 0 && ny < N && nx >= 0 && nx < N && !visit[ny][nx]) {
+              if (ny >= 0 && ny < N && nx >= 0 && nx < N && !rbVisit[ny][nx]) {
                 if (board[ny][nx] == color
                     || ((color == 'R' || color == 'G') && (board[ny][nx] == 'R' || board[ny][nx] == 'G'))) {
-                  visit[ny][nx] = true;
+                  rbVisit[ny][nx] = true;
                   bfs.offer(new int[] { ny, nx });
                 }
               }
             }
           }
         }
+
       }
     }
 
     System.out.println(rgbCount + " " + rbCount);
-    br.close();
   }
 }
