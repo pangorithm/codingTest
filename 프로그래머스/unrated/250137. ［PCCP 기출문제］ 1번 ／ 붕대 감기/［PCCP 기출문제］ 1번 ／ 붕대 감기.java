b0aef7e272221392b4effeb65aa626a00354fc18
pangHS <121858078+pangorithm@users.noreply.github.com>
2023-12-14 20:14:13 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,19 +1,12 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
         int hp = health;
-        int time = 1;
-        int bandageStack = 0;
+        int beforeAttackTime = 0;
         for(int[] attack : attacks){
-            while(time++ < attack[0]){
-                hp += bandage[1];
-                bandageStack++;
-                if(bandageStack == bandage[0]){
-                    hp += bandage[2];
-                    bandageStack = 0;
-                }
-                hp = Math.min(hp, health);
-            }
-            bandageStack = 0;
+            int timeDiff = attack[0] - beforeAttackTime - 1;
+            beforeAttackTime = attack[0];
+            hp += (timeDiff * bandage[1]) + ((timeDiff / bandage[0]) * bandage[2]);
+            hp = Math.min(hp, health);
             hp -= attack[1];
             if(hp <= 0){
                 hp = -1;
