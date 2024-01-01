6c4c3de7a43cda3e0be9bdf1a7a38671f4de9ffc
pangorithm <phs559800@gmail.com>
2024-01-01 22:13:27 +0900

    refactor: 백준 typescript 입력 방식 변경

--- 백준/template/typeScript/src/solution.ts
+++ 백준/template/typeScript/src/solution.ts
@@ -1,7 +1,17 @@
 import fs = require('fs');
-var input: string[] = fs.existsSync('./src/myTest.txt')
-  ? fs.readFileSync('./src/myTest.txt').toString().trim().split(' ')
-  : fs.readFileSync('/dev/stdin').toString().trim().split(' ');
-var a = parseInt(input[0]);
-var b = parseInt(input[1]);
-console.log(a + b);
+const input: string[][] =
+  process.platform === 'linux'
+    ? fs
+        .readFileSync('/dev/stdin')
+        .toString()
+        .trim()
+        .split('\n')
+        .map((ln) => ln.trim().split(' '))
+    : fs
+        .readFileSync('./src/myTest.txt')
+        .toString()
+        .trim()
+        .split('\n')
+        .map((ln) => ln.trim().split(' '));
+
+console.log(input);
