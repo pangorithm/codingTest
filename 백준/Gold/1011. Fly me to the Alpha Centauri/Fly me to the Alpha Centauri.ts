7b6bfacbfcb738214e8af43ad10d8a9875c05e6e
pangHS <121858078+pangorithm@users.noreply.github.com>
2024-01-06 11:19:13 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 184 ms, Memory: 9932 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.ts
@@ -0,0 +1,44 @@
+import fs = require('fs');
+const input: string[][] =
+  process.platform === 'linux'
+    ? fs
+        .readFileSync('/dev/stdin')
+        .toString()
+        .trim()
+        .split('\n')
+        .map((ln) => ln.trim().split(' '))
+    : fs
+        .readFileSync(__dirname + '/myTest.txt')
+        .toString()
+        .trim()
+        .split('\n')
+        .map((ln) => ln.trim().split(' '));
+
+let answer: string = '';
+
+const T: number = parseInt(input[0][0]);
+for (let t = 1; t <= T; t++) {
+  const x: number = parseInt(input[t][0]);
+  const y: number = parseInt(input[t][1]);
+  let move: number = 1;
+  let leftDistance: number = y - x;
+  let count: number = 0;
+  while (leftDistance > 0) {
+    if (leftDistance >= move * 2) {
+      leftDistance -= move * 2;
+      count += 2;
+    } else if (leftDistance > move) {
+      leftDistance -= move;
+      count++;
+    } else {
+      leftDistance = 0;
+      count++;
+    }
+
+    move++;
+  }
+
+  answer += count + '\n';
+}
+
+console.log(answer);
