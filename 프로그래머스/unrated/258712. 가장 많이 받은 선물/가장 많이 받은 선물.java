ea5dc72df154d6fff628cbf6f7ed5f2fcb6c9664
pangHS <121858078+pangorithm@users.noreply.github.com>
2024-01-07 22:59:43 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 3.29 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        
+        int n = friends.length;
+        HashMap<String, Integer> indexMap = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            indexMap.put(friends[i], i);
+        }
+        
+        int[][] board = new int[n][n];
+        for(String str : gifts){
+            String[] gift = str.split(" ");
+            int aIndex = indexMap.get(gift[0]);
+            int bIndex = indexMap.get(gift[1]);
+            board[aIndex][bIndex]++;
+        }
+        
+        int[] giftPoint = new int[friends.length];
+        for(int from = 0; from < n; from++){
+            for(int to = 0; to < n; to++){
+                giftPoint[from] += board[from][to];
+                giftPoint[to] -= board[from][to];
+            }
+        }
+        
+        int[] nextGifts = new int[n];
+        for(int a = 0; a < n; a++){
+            for(int b = a + 1; b < n; b++){
+                if(board[a][b] > board[b][a]){
+                    nextGifts[a]++;
+                } else if(board[a][b] < board[b][a]){
+                    nextGifts[b]++;
+                } else {
+                    if(giftPoint[a] > giftPoint[b]){
+                        nextGifts[a]++;
+                    } else if(giftPoint[a] < giftPoint[b]){
+                        nextGifts[b]++;
+                    }
+                }
+            }
+        }
+        
+        int max = 0;
+        for(int ng : nextGifts){
+            if(max < ng){
+                max = ng;
+            }
+        }
+        return max;
+    }
+}
