6ffcb255eb17470b8fa336fa1d2396aa831ec231
pangHS <121858078+pangorithm@users.noreply.github.com>
2024-01-24 21:38:02 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
 SELECT TITLE, b.BOARD_ID, REPLY_ID, r.WRITER_ID, r.CONTENTS, date_format(r.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
 from USED_GOODS_BOARD as b join USED_GOODS_REPLY as r on b.BOARD_ID = r.BOARD_ID
-where year(r.CREATED_DATE) = '2022' and month(b.CREATED_DATE) = '10'
+where year(b.CREATED_DATE) = '2022' and month(b.CREATED_DATE) = '10'
 order by r.CREATED_DATE, TITLE;
