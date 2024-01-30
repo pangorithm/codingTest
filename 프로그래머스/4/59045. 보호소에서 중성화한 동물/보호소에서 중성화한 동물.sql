2b597198856cba242b1ba279682ffdd1a78918c9
pangHS <121858078+pangorithm@users.noreply.github.com>
2024-01-30 21:51:39 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,5 +1,8 @@
 -- 코드를 입력하세요
 SELECT i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
-from animal_ins as i join animal_outs as o on i.animal_id = o.animal_id
-where i.SEX_UPON_INTAKE regexp 'Intact' and o.SEX_UPON_OUTCOME regexp 'Spayed|Neutered'
+from (
+    select * from animal_ins where SEX_UPON_INTAKE regexp 'Intact'
+) as i join (
+    select * from animal_outs where SEX_UPON_OUTCOME regexp 'Spayed|Neutered'
+) as o on i.animal_id = o.animal_id
 order by i.animal_id
