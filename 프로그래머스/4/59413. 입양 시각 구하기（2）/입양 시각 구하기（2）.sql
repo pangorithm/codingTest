6581d08f6dba9577ff0f0c684f1f14b984353111
pangHS <121858078+pangorithm@users.noreply.github.com>
2024-01-31 22:34:46 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+set @hour = -1;
+SELECT (@hour := @hour + 1) as HOUR, (select count(hour(datetime)) from animal_outs where hour(datetime) = @hour) as COUNT
+from animal_outs
+where @hour < 23;
