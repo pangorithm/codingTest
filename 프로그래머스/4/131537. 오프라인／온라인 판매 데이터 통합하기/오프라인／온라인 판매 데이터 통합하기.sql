41dff6c9466f07c2ead77d2ef6b3f3feed558077
pangHS <121858078+pangorithm@users.noreply.github.com>
2024-01-31 22:25:50 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+(
+    select
+        date_format(SALES_DATE, '%Y-%m-%d') as SALES_DATE,
+        PRODUCT_ID, 
+        USER_ID, 
+        SALES_AMOUNT 
+    from ONLINE_SALE 
+    where SALES_DATE like '2022-03%'
+    union 
+    select 
+        date_format(SALES_DATE, '%Y-%m-%d') as SALES_DATE, 
+        PRODUCT_ID, 
+        null as USER_ID, 
+        SALES_AMOUNT 
+    from OFFLINE_SALE 
+    where SALES_DATE like '2022-03%'
+)
+order by SALES_DATE, PRODUCT_ID, USER_ID  
