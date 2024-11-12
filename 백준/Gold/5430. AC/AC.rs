a808f039266f23398b8f4375260b574be7400826
pangHS <121858078+pangorithm@users.noreply.github.com>
2024-11-12 21:45:50 +0900

    [Gold V] Title: AC, Time: 44 ms, Memory: 21756 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.rs
@@ -0,0 +1,68 @@
+use std::collections::VecDeque;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace();
+
+    let t: i32 = input.next().unwrap().parse::<i32>().unwrap();
+
+    let mut output = String::new();
+
+    for _ in 0..t {
+        let cmd = input.next().unwrap();
+        let length: i32 = input.next().unwrap().parse::<i32>().unwrap();
+        let mut dq: VecDeque<i32> = input
+            .next()
+            .unwrap()
+            .trim_start_matches("[")
+            .trim_end_matches("]")
+            .split(",")
+            .flat_map(|s| s.parse::<i32>().ok())
+            .collect();
+
+        let mut str = "";
+
+        let mut is_reverse = false;
+        for char in cmd.chars() {
+            match char {
+                'R' => is_reverse = !is_reverse,
+                'D' => {
+                    if dq.is_empty() {
+                        str = "error";
+                        break;
+                    }
+                    if is_reverse {
+                        let _ = dq.pop_back();
+                    } else {
+                        let _ = dq.pop_front();
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        if is_reverse {
+            dq.make_contiguous().reverse();
+        }
+
+        if str == "" {
+            writeln!(
+                output,
+                "[{}]",
+                dq.iter()
+                    .cloned()
+                    .map(|n| n.to_string())
+                    .collect::<Vec<String>>()
+                    .join(",")
+            )
+            .unwrap();
+        } else {
+            writeln!(output, "{}", str).unwrap();
+        }
+    }
+
+    println!("{}", output);
+}
