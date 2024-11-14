d039bfae31599d13bceecd29883f9ff3bfb2164e
pangorithm <phs559800@gmail.com>
2024-11-14 20:20:55 +0900

    템플릿 수정

--- 백준/template/rust/src/main.rs
+++ 백준/template/rust/src/main.rs
@@ -2,14 +2,14 @@ use std::fmt::Write;
 use std::io::{stdin, Read};
 
 fn main() {
+    let mut output = String::new();
+
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-    let input = input.split_ascii_whitespace();
+    let mut input = input.split_ascii_whitespace();
 
-    // let n = input.next().unwrap().parse::<i32>().unwrap();
-    // let word = input.next().unwrap();
-
-    let mut output = String::new();
+    let n = input.next().unwrap().parse::<i32>().unwrap();
+    let word = input.next().unwrap();
 
     for word in input {
         write!(output, "{}\n", word).unwrap();
